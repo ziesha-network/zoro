8cfa808ee1e7e03c8c62a087ddc81b6574ceac17
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2022-09-04 20:50:44 +0430

    Update circuit sizes

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
-pub const LOG4_TREE_SIZE: u8 = 10;
-pub const LOG4_PAYMENT_BATCH_SIZE: u8 = 1;
-pub const LOG4_UPDATE_BATCH_SIZE: u8 = 1;
+pub const LOG4_TREE_SIZE: u8 = 15;
+pub const LOG4_PAYMENT_BATCH_SIZE: u8 = 3;
+pub const LOG4_UPDATE_BATCH_SIZE: u8 = 4;
