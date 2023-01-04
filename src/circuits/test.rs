15c04bc1baa167533e0046881574bdec432034bb
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-01-04 20:49:03 +0330

    Check withdraw circuit correct state-change

--- src/circuits/test.rs
+++ src/circuits/test.rs
@@ -229,3 +229,92 @@ fn test_withdraw_empty() {
         .prove()
         .unwrap();
 }
+
+#[test]
+fn test_withdraw_tx() {
+    let tx_builder = TxBuilder::new(b"hi");
+    let zk_addr = tx_builder.get_zk_address().decompress();
+    let (mut db, mpn_contract_id) = fresh_db(1, 1);
+    let deposit_circ = DepositCircuit::<1, 1, 1>::default();
+    let withdraw_circ = WithdrawCircuit::<1, 1, 1>::default();
+    let param_deposit =
+        groth16::generate_random_parameters::<Bls12, _, _>(deposit_circ, &mut OsRng).unwrap();
+    let param_withdraw =
+        groth16::generate_random_parameters::<Bls12, _, _>(withdraw_circ, &mut OsRng).unwrap();
+    let b = Bank::<1, 0, 1, 1, 1>::new(mpn_contract_id, false, true);
+    let d = Deposit {
+        mpn_deposit: None,
+        index: 2,
+        token_index: 3,
+        pub_key: zk_addr.clone(),
+        amount: (TokenId::Custom(ZkScalar::from(123)), Money(10)),
+    };
+    let (acc, rej, _, work) = b
+        .deposit(
+            &mut db,
+            param_deposit.clone(),
+            vec![d],
+            Arc::new(RwLock::new(false)),
+        )
+        .unwrap();
+    assert_eq!(acc.len(), 1);
+    assert_eq!(rej.len(), 0);
+    work.prove().unwrap();
+    let wt = tx_builder.withdraw_mpn(
+        mpn_contract_id,
+        2,
+        0,
+        3,
+        TokenId::Custom(ZkScalar::from(123)),
+        Money(2),
+        3,
+        TokenId::Custom(ZkScalar::from(123)),
+        Money(3),
+    );
+    let w = Withdraw {
+        mpn_withdraw: None,
+        nonce: 0,
+        pub_key: tx_builder.get_zk_address().0.decompress(),
+        token_index: 3,
+        fingerprint: wt.payment.fingerprint(),
+        index: 2,
+        fee: (TokenId::Custom(ZkScalar::from(123)), Money(3)),
+        fee_token_index: 3,
+        amount: (TokenId::Custom(ZkScalar::from(123)), Money(2)),
+        sig: wt.zk_sig.clone(),
+    };
+    let (acc, rej, _, work) = b
+        .withdraw(
+            &mut db,
+            param_withdraw.clone(),
+            vec![w],
+            Arc::new(RwLock::new(false)),
+        )
+        .unwrap();
+    assert_eq!(acc.len(), 1);
+    assert_eq!(rej.len(), 0);
+    //work.prove().unwrap();
+    let state = KvStoreStateManager::<bazuka::core::ZkHasher>::get_full_state(&db, mpn_contract_id)
+        .unwrap();
+    assert_eq!(state.data.0.len(), 5);
+    assert_eq!(
+        state.data.0.get(&ZkDataLocator(vec![2, 0])),
+        Some(&1.into())
+    );
+    assert_eq!(
+        state.data.0.get(&ZkDataLocator(vec![2, 1])),
+        Some(&zk_addr.0)
+    );
+    assert_eq!(
+        state.data.0.get(&ZkDataLocator(vec![2, 2])),
+        Some(&zk_addr.1)
+    );
+    assert_eq!(
+        state.data.0.get(&ZkDataLocator(vec![2, 3, 3, 0])),
+        Some(&ZkScalar::from(123))
+    );
+    assert_eq!(
+        state.data.0.get(&ZkDataLocator(vec![2, 3, 3, 1])),
+        Some(&ZkScalar::from(5))
+    );
+}
