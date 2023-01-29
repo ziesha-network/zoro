b975aa1e0c54cce26586674b10040454725ecfe1
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-01-29 14:30:01 +0330

    Previous tree size

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub const LOG4_TREE_SIZE: u8 = 10;
+pub const LOG4_TREE_SIZE: u8 = 15;
 pub const LOG4_TOKENS_TREE_SIZE: u8 = 1;
 pub const LOG4_DEPOSIT_BATCH_SIZE: u8 = 1;
 pub const LOG4_WITHDRAW_BATCH_SIZE: u8 = 1;
