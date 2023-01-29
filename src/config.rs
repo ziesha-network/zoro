4c7c802e3acca0ebf110534ff72817d1571c844a
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-01-29 14:26:43 +0330

    Sandbox config

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-pub const LOG4_TREE_SIZE: u8 = 15;
-pub const LOG4_TOKENS_TREE_SIZE: u8 = 3;
-pub const LOG4_DEPOSIT_BATCH_SIZE: u8 = 3;
-pub const LOG4_WITHDRAW_BATCH_SIZE: u8 = 3;
-pub const LOG4_UPDATE_BATCH_SIZE: u8 = 4;
+pub const LOG4_TREE_SIZE: u8 = 10;
+pub const LOG4_TOKENS_TREE_SIZE: u8 = 1;
+pub const LOG4_DEPOSIT_BATCH_SIZE: u8 = 1;
+pub const LOG4_WITHDRAW_BATCH_SIZE: u8 = 1;
+pub const LOG4_UPDATE_BATCH_SIZE: u8 = 1;
