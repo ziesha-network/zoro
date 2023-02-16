25bd8f4197b42ec187958d790f0793720920da0e
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2023-02-16 19:33:28 +0330

    Super update circuit

--- src/config.rs
+++ src/config.rs
@@ -2,4 +2,4 @@ pub const LOG4_TREE_SIZE: u8 = 15;
 pub const LOG4_TOKENS_TREE_SIZE: u8 = 3;
 pub const LOG4_DEPOSIT_BATCH_SIZE: u8 = 3;
 pub const LOG4_WITHDRAW_BATCH_SIZE: u8 = 3;
-pub const LOG4_UPDATE_BATCH_SIZE: u8 = 4;
+pub const LOG4_UPDATE_BATCH_SIZE: u8 = 5;
